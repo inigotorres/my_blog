f1b05f4d9fc9208b6c463aff0dd70a072044fdbe
Vagrant User <vagrant@squeeze-rbenv-amd64.ptvlc>
2015-04-20 14:53:16 +0000

    Basic web with static home and about

---
+++ my_blog/app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  # association between def home and home view file
+  # controllers do this automatically  
+
+  def home
+  end
+
+  def about
+  end
+end
